294436e2bda99d213c1f4dcdf469ce33919895f7
StevenNimble <stadenoia@gmail.com>
2022-04-27 08:39:33 -0500

    added circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,7 +4,7 @@ var map = L.map('map', {
     center: [36, -118],
     zoom: 7
 });
-    // .setView([36, -118], 7);
+
 
 // Add a basic map to the window
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,23 +13,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Add earthquake data to the map
 // Need to loop JSON and find data points
-
 // import JSON data  - This pulls the data
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
-// d3.json(url).then(data => {
-//     console.log(data)
-//     console.log(data[0])
-// })
+// url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
 
 // Experimnetinig with pulling data locally.
-// url = 'earthquake.json'
+url = 'earthquake.json'
 d3.json(url).then(function(data) {
 
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
-    // console.log(data)
-    console.log(data.features[0].geometry.coordinates)  // Path to coordinates
+    // console.log(data.features[0])  // Path to coordinates
     // console.log(data.features[0].properties.mag)        // Magnitude of earthquake
     // For loop pulling all the data out of json -
     var markers = L.markerClusterGroup();
@@ -38,10 +29,45 @@ d3.json(url).then(function(data) {
     for (let i = 0; i < data.features.length; i++) {
         // console.log(data.features[i].geometry.coordinates[0] + " " + data.features[i].properties.mag)
         var location = data.features[i].geometry.coordinates
-        console.log(location[0] + "  " + location[1] + "  " + location[2])
+        var magitude = data.features[i].properties.mag  // Size
+        var depth = location[2]                         // Color
+        var color_depth = 'FFFFFF'
+        // console.log(location[0] + "  " + location[1] + "  " + location[2])
 
-        markers.addLayer(L.marker([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]])
-            .bindPopup(location[2].toString()));
+        // Make Circles pretty  neon green, lime green, light orange, dark orange,  orange red, red
+        // Depth is the color and size  is mag
+        if (depth < 10) {
+            color_depth = '#ABEA64';
+        } else if (depth < 30) {
+            color_depth = '#78AE3C'
+        } else if (depth < 50) {
+            color_depth = '#FFC300'
+        } else if (depth < 70) {
+            color_depth = '#DE942F'
+        } else if (depth < 90) {
+            color_depth = "#FF5733"
+        } else {
+            color_depth = '#C70039 '
+        }
+
+
+
+
+        // Need Circles
+        L.circle([location[1], location[0]], {
+            color: color_depth,
+            radius: magitude * 8000,
+            fillOpacity: .5
+        }).bindPopup("<b>Place:</b> " + data.features[i].properties.place + 
+        "<br><b>Depth:</b> " +  location[2] + 
+        "<br><b>Magitude:</b> " + data.features[i].properties.mag).addTo(map);
+    
+
+
+        // markers.addLayer(L.marker([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]])
+            // .bindPopup("<b>Place:</b> " + data.features[i].properties.place + 
+            // "<br><b>Depth:</b> " +  location[2] + 
+            // "<br><b>Magitude:</b> " + data.features[i].properties.mag));
         
           
     }
